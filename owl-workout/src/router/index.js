fab8eeb08e45d90395c4ef6823bc263dc9842da8
Charlie <charlie.nguyenvanphuc@gmail.com>
2019-03-30 20:10:35 +0100

    Added Firebase to app

--- owl-workout/src/router/index.js
+++ owl-workout/src/router/index.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Exercises from '@/components/Exercises'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     },
     {
-      path: '/exercises',
+      path: '/',
       name: 'Exercises',
       component: Exercises
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
